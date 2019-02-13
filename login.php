3b6d8d6157cd8af40a40fb606a791651126d5338
Willian Trevisan <williantrev@gmail.com>
2019-02-13 20:31:30 -0300

    Tela de login criada(login.php)

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head> 	
+ 	<title>Tela de Login</title>
+ 	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+ </head>
+ <body>
+	<div class="retangulo">
+		<img src="images/logo.png" class="logo_login"/>
+		<form name="login" method="GET">
+			<input type="text" placeholder="Login">
+			<input type="text" placeholder="Senha">	
+			<button type="submit" name="enviar" class="button_login">ENTRAR</button>		
+		</form>
+	</div> 
+ </body>
+ </html>
