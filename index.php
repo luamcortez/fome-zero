372f77f04fca1587b294a1613d62cb0ecf70bbfa
Willian Trevisan <williantrev@gmail.com>
2019-02-14 14:23:54 -0300

    Index adicionado

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
-	include 'conexao.php';
+	//include 'conexao.php';
  ?>
+ 
+  <!DOCTYPE html>
+ <html>
+ <head> 	
+ 	<title>Tela de Login</title>
+ 	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+ </head>
+ <body>
+	<div class="retangulo">
+		<img src="images/logo.png" class="logo_login"/>
+		<form name="login" method="GET">
+			<button type="submit" name="filapedido" class="button_login">FILA DE PEDIDOS</button>
+			<button type="submit" name="cliente" class="button_login">CLIENTE</button>	
+			<button type="submit" name="editarcardapio" class="button_login">EDITAR CARDAPIO</button>		
+		</form>
+	</div> 
+ </body>
+ </html>
