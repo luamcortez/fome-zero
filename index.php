f335b69d396956e6b45c033c05ad95f3f2ec6445
luamcb <luamcortez@gmail.com>
2019-02-19 16:53:24 -0400

    Reseter e botões do menu com o tag <a>

--- index.php
+++ index.php
@@ -6,16 +6,17 @@
  <html>
  <head> 	
  	<title>Menu</title>
+ 	<link rel="stylesheet" type="text/css" href="css/reseter.css">
  	<link rel="stylesheet" type="text/css" href="css/style.css">
-
+ 	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
  </head>
  <body>
 	<div class="retangulo">
 		<img src="images/logo.png" class="logo_login"/>
-		<form name="login" method="GET">
-			<button type="submit" name="filapedido" class="button_login">FILA DE PEDIDOS</button>
-			<button type="submit" name="cliente" class="button_login">CLIENTE</button>	
-			<button type="submit" name="editarcardapio" class="button_login">EDITAR CARDAPIO</button>		
+		<form id="menu" name="login" method="GET">
+			<a class="button_login" href="fila_pedidos.html">FILA DE PEDIDOS</a>
+			<a class="button_login" href="cliente.html">CLIENTE</a>	
+			<a class="button_login" href="editar_cardapio.html">EDITAR CARDAPIO</a>		
 		</form>
 	</div> 
  </body>
