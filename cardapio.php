60c89c2b52aa720b09f9ceadb4155fbec7d6709b
luamcb <luamcortez@gmail.com>
2019-02-19 17:31:18 -0400

    Criação da Tela do Cardápio

---
+++ cardapio.php
@@ -0,0 +1,17 @@
+<?php
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head> 	
+ 	<title>Cardápio</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+		<style>
+			
+		</style>
+</head>
+ <body class="fundo-cardapio">
+
+ </body>
+ </html>
