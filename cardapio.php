247a9aaaf5251976e6b9cca17006f2680b7eab6e
luamcb <luamcortez@gmail.com>
2019-02-27 00:11:23 -0400

    Progresso no Cardápio

--- cardapio.php
+++ cardapio.php
@@ -7,51 +7,224 @@
  <head> 	
  	<title>Cardápio</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
-		<style>
-			
-		</style>
+	<link href="https://fonts.googleapis.com/css?family=Crimson+Text" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=2.0">
+
 </head>
  <body class="fundo-cardapio">
 	<div class="container">
 		<div class="logo-cardapio"></div>
+		<div class="button-pedidos">
+			<img class="icone-recibo" src="documentacao/assets/icone-recibo.png" alt="">
+			<a>Pedidos</a>
+			<div class="circulo-numero"><p>15</p></div>
+		</div>
 		<div class="container-categoria">
-			<div class="button-categoria"></div>
-			<div class="button-categoria"></div>
-			<div class="button-categoria"></div>
-			<div class="button-categoria"></div>
-			<div class="button-categoria"></div>
-			<div class="button-categoria"></div>
-			<div class="button-categoria"></div>
+			<div class="conteudo-categorias">
+				<button class="button-categoria">Categoria 1</button>
+				<button class="button-categoria">Categoria 2</button>
+				<button class="button-categoria">Categoria 3</button>
+				<button class="button-categoria">Categoria 4</button>
+				<button class="button-categoria">Categoria 5</button>
+				<button class="button-categoria">Categoria 6</button>
+				<button class="button-categoria">Categoria 7</button>
+				<button class="button-categoria">Categoria 8</button>
+			</div>
 		</div>
 		<div class="container-itens">
 			<div class="conteudo-itens">
+
 				<div class="item">
-					<div class="item-foto"></div>
-					<h2>Item 1</h2>
+					<img class="item-foto" src="https://source.unsplash.com/featured/?food">
+					<div class="item-button">
+						<p>R$20,00</p>
+						<div class="circulo"><a>+</a></div>
+					</div>
+					<p class="item-titulo">Título</p>
+					<div class="item-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Sed id ipsum mi. Aliquam in arcu eleifend, venenatis leo fermentum, egestas orci.
+						Nunc pulvinar tortor ut enim sodales accumsan.
+					</div>
 				</div>
+
 				<div class="item">
-					<div class="item-foto"></div>
+				<img class="item-foto" src="https://source.unsplash.com/featured/?food">
+					<div class="item-button">
+						<p>R$20,00</p>
+						<div class="circulo"><a>+</a></div>
+					</div>
+					<p class="item-titulo">Título</p>
+					<div class="item-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Sed id ipsum mi. Aliquam in arcu eleifend, venenatis leo fermentum, egestas orci.
+						Nunc pulvinar tortor ut enim sodales accumsan.
+					</div>
 				</div>
+
 				<div class="item">
-					<div class="item-foto"></div>
+				<img class="item-foto" src="https://source.unsplash.com/featured/?food">
+					<div class="item-button">
+						<p>R$20,00</p>
+						<div class="circulo"><a>+</a></div>
+					</div>
+					<p class="item-titulo">Título</p>
+					<div class="item-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Sed id ipsum mi. Aliquam in arcu eleifend, venenatis leo fermentum, egestas orci.
+						Nunc pulvinar tortor ut enim sodales accumsan.
+					</div>
 				</div>
+
 				<div class="item">
-					<div class="item-foto"></div>
+				<img class="item-foto" src="https://source.unsplash.com/featured/?food">
+					<div class="item-button">
+						<p>R$20,00</p>
+						<div class="circulo"><a>+</a></div>
+					</div>
+					<p class="item-titulo">Título</p>
+					<div class="item-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Sed id ipsum mi. Aliquam in arcu eleifend, venenatis leo fermentum, egestas orci.
+						Nunc pulvinar tortor ut enim sodales accumsan.
+					</div>
 				</div>
+
 				<div class="item">
-					<div class="item-foto"></div>
+				<img class="item-foto" src="https://source.unsplash.com/featured/?food">
+					<div class="item-button">
+						<p>R$20,00</p>
+						<div class="circulo"><a>+</a></div>
+					</div>
+					<p class="item-titulo">Título</p>
+					<div class="item-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Sed id ipsum mi. Aliquam in arcu eleifend, venenatis leo fermentum, egestas orci.
+						Nunc pulvinar tortor ut enim sodales accumsan.
+					</div>
 				</div>
+
 				<div class="item">
-					<div class="item-foto"></div>
+				<img class="item-foto" src="https://source.unsplash.com/featured/?food">
+					<div class="item-button">
+						<p>R$20,00</p>
+						<div class="circulo"><a>+</a></div>
+					</div>
+					<p class="item-titulo">Título</p>
+					<div class="item-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Sed id ipsum mi. Aliquam in arcu eleifend, venenatis leo fermentum, egestas orci.
+						Nunc pulvinar tortor ut enim sodales accumsan.
+					</div>
 				</div>
+
 				<div class="item">
-					<div class="item-foto"></div>
+				<img class="item-foto" src="https://source.unsplash.com/featured/?food">
+					<div class="item-button">
+						<p>R$20,00</p>
+						<div class="circulo"><a>+</a></div>
+					</div>
+					<p class="item-titulo">Título</p>
+					<div class="item-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Sed id ipsum mi. Aliquam in arcu eleifend, venenatis leo fermentum, egestas orci.
+						Nunc pulvinar tortor ut enim sodales accumsan.
+					</div>
 				</div>
+
 				<div class="item">
-					<div class="item-foto"></div>
+				<img class="item-foto" src="https://source.unsplash.com/featured/?food">
+					<div class="item-button">
+						<p>R$20,00</p>
+						<div class="circulo"><a>+</a></div>
+					</div>
+					<p class="item-titulo">Título</p>
+					<div class="item-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Sed id ipsum mi. Aliquam in arcu eleifend, venenatis leo fermentum, egestas orci.
+						Nunc pulvinar tortor ut enim sodales accumsan.
+					</div>
 				</div>
+
+				<div class="item">
+				<img class="item-foto" src="https://source.unsplash.com/featured/?food">
+					<div class="item-button">
+						<p>R$20,00</p>
+						<div class="circulo"><a>+</a></div>
+					</div>
+					<p class="item-titulo">Título</p>
+					<div class="item-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Sed id ipsum mi. Aliquam in arcu eleifend, venenatis leo fermentum, egestas orci.
+						Nunc pulvinar tortor ut enim sodales accumsan.
+					</div>
+				</div>
+
+				<div class="item">
+				<img class="item-foto" src="https://source.unsplash.com/featured/?food">
+					<div class="item-button">
+						<p>R$20,00</p>
+						<div class="circulo"><a>+</a></div>
+					</div>
+					<p class="item-titulo">Título</p>
+					<div class="item-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Sed id ipsum mi. Aliquam in arcu eleifend, venenatis leo fermentum, egestas orci.
+						Nunc pulvinar tortor ut enim sodales accumsan.
+					</div>
+				</div>
+
+				<div class="item">
+				<img class="item-foto" src="https://source.unsplash.com/featured/?food">
+					<div class="item-button">
+						<p>R$20,00</p>
+						<div class="circulo"><a>+</a></div>
+					</div>
+					<p class="item-titulo">Título</p>
+					<div class="item-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Sed id ipsum mi. Aliquam in arcu eleifend, venenatis leo fermentum, egestas orci.
+						Nunc pulvinar tortor ut enim sodales accumsan.
+					</div>
+				</div>
+
+				<div class="item">
+				<img class="item-foto" src="https://source.unsplash.com/featured/?food">
+					<div class="item-button">
+						<p>R$20,00</p>
+						<div class="circulo"><a>+</a></div>
+					</div>
+					<p class="item-titulo">Título</p>
+					<div class="item-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Sed id ipsum mi. Aliquam in arcu eleifend, venenatis leo fermentum, egestas orci.
+						Nunc pulvinar tortor ut enim sodales accumsan.
+					</div>
+				</div>
+
+				<div class="item">
+				<img class="item-foto" src="https://source.unsplash.com/featured/?food">
+					<div class="item-button">
+						<p>R$20,00</p>
+						<div class="circulo"><a>+</a></div>
+					</div>
+					<p class="item-titulo">Título</p>
+					<div class="item-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Sed id ipsum mi. Aliquam in arcu eleifend, venenatis leo fermentum, egestas orci.
+						Nunc pulvinar tortor ut enim sodales accumsan.
+					</div>
+				</div>
+
+				<div class="item">
+				<img class="item-foto" src="https://source.unsplash.com/featured/?food">
+					<div class="item-button">
+						<p>R$20,00</p>
+						<div class="circulo"><a>+</a></div>
+					</div>
+					<p class="item-titulo">Título</p>
+					<div class="item-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Sed id ipsum mi. Aliquam in arcu eleifend, venenatis leo fermentum, egestas orci.
+						Nunc pulvinar tortor ut enim sodales accumsan.
+					</div>
+				</div>
+
+				
 			</div>
 		</div>
 	</div>
+	<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+	<!-- <script type="text/javascript">
+		$(document).on('click','button-categoria', function()){
+			$(this).addClass('active')
+		}
+	</script> -->
 </body>
  </html>
