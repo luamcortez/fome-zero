ae84cdc18adeaee3e7d1759db15abc2559274794
Alexandre Lucas <xandelucas@gmail.com>
2019-03-07 15:48:07 -0400

    Tela de pedidos iniciada

---
+++ Tela_pedidos.php
@@ -0,0 +1,20 @@
+<?php 
+
+?>
+
+
+
+
+<!DOCTYPE html>
+ <html>
+ <head> 	
+ 	<title>Tela de Pedidos</title>
+ 	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+ </head>
+ <body>
+	<div class="retangulo">
+		
+	</div> 
+ </body>
+ </html>
