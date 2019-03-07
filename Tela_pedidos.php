2dd44a59cdfa74c9d9e397da2b89a5ea062010a0
Alexandre Lucas <xandelucas@gmail.com>
2019-03-07 17:27:34 -0400

    Esqueleto dos pedidos criado

--- Tela_pedidos.php
+++ Tela_pedidos.php
@@ -1,5 +1,5 @@
 <?php 
-
+	
 ?>
 
 
@@ -10,10 +10,40 @@
  <head> 	
  	<title>Tela de Pedidos</title>
  	<link rel="stylesheet" type="text/css" href="css/style.css">
-
+	<link rel="stylesheet" type="text/css" href="css/reseter.css">
+	 <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	 <meta name="viewport" content="width=device-width, initial-scale=2.0">
  </head>
+
  <body>
-	<div class="retangulo">
+	<div class="grid-container">
+		<div class="grid-item">1
+
+		</div>
+  		<div class="grid-item">2
+
+		</div>
+  		<div class="grid-item">3
+
+		</div>  
+  		<div class="grid-item">4
+
+		</div>
+  		<div class="grid-item">5
+
+		</div>
+  		<div class="grid-item">6
+
+		</div> 
+		<div class="grid-item">7
+
+		</div>
+		<div class="grid-item">8
+			
+		</div>
+		
+
+		 
 		
 	</div> 
  </body>
